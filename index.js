bd7b69c8e8f4d2246d0cd80c2f6492509993b44c
Estiven Mayhuay <developerwebhairton@gmail.com>
2022-12-01 14:51:08 -0500

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 let audio = $("#audioLiliana");
+let mainContent = $("#app");
 
 $(document).on("click", "#startPresentation", function () {
+  // Play Audio
   $(audio)[0].play();
+
+  $(mainContent);
+
+  setTimeout(() => {}, 10000);
 });
