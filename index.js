8e8483e387f73a47940e3376d56232b38f4c71fe
Estiven Mayhuay <developerwebhairton@gmail.com>
2022-12-01 15:25:47 -0500

    second commit

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 let audio = $("#audioLiliana");
 let mainContent = $("#app");
-let counter = 11;
+let counter = 2;
 
 $(document).on("click", "#startPresentation", function () {
   // Play Audio
   $(audio)[0].play();
 
   $("#mainCard").hide(200);
+  $("#counter").removeClass("d-none");
 
-  setInterval(() => {
+  let dataInterval = setInterval(() => {
     counter -= 1;
     console.log(counter);
     $("#counter h1").text(counter);
+
+    if (counter < 0) {
+      clearInterval(dataInterval);
+      $("#counter").addClass("d-none");
+      $("#quienEsCard").removeClass("d-none");
+    }
   }, 1001);
+});
+
+$(document).on("click", "#goToGustar", function () {
+  $("#quienEsCard").addClass("d-none");
+  $("#gustaDeLilianaCard").removeClass("d-none");
 });
